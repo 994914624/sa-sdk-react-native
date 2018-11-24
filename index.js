b43c87e6a80c8dab14523a2ff5fd131449619c73
yangzhankun <yangzhankun@sensorsdata.cn>
2018-11-24 15:16:46 +0800

    b

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-function hello() {
-        console.log("hello hello");
-}
+//导入神策 Android 模块 用于代码埋点
+import { NativeModules } from 'react-native';
+const RNSensorsAnalyticsModule=NativeModules.RNSensorsAnalyticsModule;
